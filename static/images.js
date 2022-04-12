4f2381fdb626b32c2074f8e8938f04b955a73d5e
Oriol Carbo <oriol.carbo@gmail.com>
2022-04-13 01:34:18 +0200

    style: add button icons

--- static/images.js
+++ static/images.js
@@ -1,4 +1,6 @@
 const collaborators = require('../images/collaboratorsIcon.png');
 const link = require('../images/linkIcon.png');
+const list = require('../images/listIcon.png');
+const grid = require('../images/gridIcon.png');
 
-export { collaborators, link };
+export { collaborators, link, list, grid };

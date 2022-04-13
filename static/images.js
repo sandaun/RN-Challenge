3c08aa6ddd9eed394e27f927d6bd2bf4a019657e
Oriol Carbo <oriol.carbo@gmail.com>
2022-04-13 22:49:40 +0200

    style: add notification icon

--- static/images.js
+++ static/images.js
@@ -2,5 +2,6 @@ const collaborators = require('../images/collaboratorsIcon.png');
 const link = require('../images/linkIcon.png');
 const list = require('../images/listIcon.png');
 const grid = require('../images/gridIcon.png');
+const bell = require('../images/bellIcon.png');
 
-export {collaborators, link, list, grid};
+export {collaborators, link, list, grid, bell};

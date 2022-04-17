cb7add9174a9ab138d84d69768237de99bb20c77
Oriol Carbó <oriol.carbo@rideyego.com>
2022-04-17 20:29:22 +0200

    add file icon

--- static/images.js
+++ static/images.js
@@ -3,5 +3,6 @@ const link = require('../images/linkIcon.png');
 const list = require('../images/listIcon.png');
 const grid = require('../images/gridIcon.png');
 const bell = require('../images/bellIcon.png');
+const document = require('../images/documentIcon.png');
 
-export {collaborators, link, list, grid, bell};
+export {collaborators, link, list, grid, bell, document};

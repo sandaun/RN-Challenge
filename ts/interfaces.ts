6f9bf8ea424297d2756a0138212ab66e33e4e941
Oriol Carbó <oriol.carbo@rideyego.com>
2022-10-14 09:15:45 +0200

    chore: create interfaces.ts

---
+++ ts/interfaces.ts
@@ -0,0 +1,42 @@
+export interface Doc {
+  Title: string;
+  Version: string;
+  Contributors: Contributor[];
+  Attachments: string[];
+  CreatedAt: string;
+  UpdatedAt: string;
+  ID: string;
+}
+
+export interface Contributor {
+  ID: number;
+  Name: string;
+}
+
+export interface BottomModalRef {
+  open: () => void;
+  close: () => void;
+}
+
+export interface Notification {
+  Timestamp: string;
+  DocumentTitle: string;
+}
+
+export interface File {
+  Attachments: string[];
+  Contributors: Contributor[];
+  ID: number;
+}
+
+export interface AppContext<T> {
+  contextData?: T;
+  setContextData?: (cb: (value: T) => T) => void;
+}
+
+export interface ContextData {
+  Notifications: Notification[],
+  Documents: Partial<Doc>[],
+  Attachments: string[];
+  Contributors: Contributor[];
+}

e5c11369e5fe7aa51258389a2f431794bcb02118
Oriol Carbó <oriol.carbo@rideyego.com>
2022-10-14 08:39:09 +0200

    wip: transform to TSX in progress

---
+++ context/ChallengeProvider.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import { AppContext, ContextData } from '../ts/interfaces';
+
+export const ChallengeContext = React.createContext<AppContext<ContextData>>({});
+
+export type Props = {
+  children: React.ReactNode;
+};
+
+const ChallengeProvider: React.FC<Props> = props => {
+  // const [contextData, setContextData] = useState<ContextData>((prevContext: ContextData): ContextData => {
+    const [contextData, setContextData] = useState<ContextData>((): ContextData => {
+    return {
+      // ...prevContext,
+      Notifications: [],
+      Documents: [],
+      Attachments: [],
+      Contributors: []
+    };
+  });
+
+  return (
+    <ChallengeContext.Provider
+      value={{
+        contextData,
+        setContextData,
+      }}>
+      {props.children}
+    </ChallengeContext.Provider>
+  );
+};
+
+export default ChallengeProvider;
